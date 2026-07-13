987e4e4418485d799b76e8d050b4ea91c0727eb3
michaelkolet101 <michael24kolwr@gmail.com>
2026-07-13 14:15:15 +0300

    fix some bags

--- Store Product Management System/src/App.jsx
+++ Store Product Management System/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // מחקנו את ה-Router מהייבוא
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Products from './pages/Products';
@@ -8,10 +8,28 @@ import About from './pages/About';
 import './App.css';
 
 function App() {
-  
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => {
+        const formatted = data.map(p => ({
+          id: p.id,
+          name: p.title,
+          price: p.price,
+          image: p.image,
+          inStock: true,
+          isLiked: false
+        }));
+        setProducts(formatted);
+      });
+  }, []);
+
+  const addProduct = (p) => setProducts([...products, p]);
 
   return (
-    <Router>
+    <> {/* תגית ריקה במקום ה-Router */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -20,7 +38,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="*" element={<h1>404 - דף לא נמצא</h1>} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
